d01dc1e509b6009cca565880912fd22bf8f923aa
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-14 14:26:38 +0100

    add age Will need to add 0-3 months and 3 months-5 years

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -28,3 +28,35 @@ study = StudyDefinition(
         return_expectations={"incidence": 0.1}
         ),
     ),
+    age=patients.age_as_of(
+        "index_date",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+
+    age_band=patients.categorised_as(
+        {
+            "missing": "DEFAULT",
+            "0-5": """ age >= 0 AND age <= 5""",
+            "6-10": """ age >  5 AND age <= 10""",
+            "11-20": """ age > 10 AND age <= 20""",
+            "21-30": """ age > 20 AND age <= 30""",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "missing": 0.05,
+                    "0-5": 0.25,
+                    "6-10": 0.25,
+                    "11-20": 0.25,
+                    "21-30": 0.2
+                }
+            },
+        },
+
+    ),
+)
+
