6438d80ddbf282ad7ed5bcab2efecb360b64dd6b
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-14 15:25:06 +0100

    separate study_def for weekly extract

--- analysis/study_definition_weekly.py
+++ analysis/study_definition_weekly.py
@@ -9,6 +9,7 @@ study = StudyDefinition(
         "rate": "exponential_increase",
         "incidence": 0.1,
     },
+    
     population=patients.satisfying(
         """
         registered AND
@@ -16,14 +17,16 @@ study = StudyDefinition(
         (age >=0 AND age <=30) AND 
         (sex = 'M' OR sex = 'F')
         """,
-        registered=patients.registered_as_of(
-            "index_date",
-            return_expectations={"incidence": 0.9},
+
+        registered = patients.registered_as_of(
+        "index_date",
+        return_expectations={"incidence": 0.9},
         ),
-        died=patients.died_from_any_cause(
-            on_or_before="index_date",
-            returning="binary_flag",
-            return_expectations={"incidence": 0.1},
+
+        died = patients.died_from_any_cause(
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence": 0.1}
         ),
     ),
     age=patients.age_as_of(
@@ -33,6 +36,7 @@ study = StudyDefinition(
             "int": {"distribution": "population_ages"},
         },
     ),
+
     age_band=patients.categorised_as(
         {
             "missing": "DEFAULT",
@@ -49,10 +53,11 @@ study = StudyDefinition(
                     "0-5": 0.25,
                     "6-10": 0.25,
                     "11-20": 0.25,
-                    "21-30": 0.2,
+                    "21-30": 0.2
                 }
             },
         },
+
     ),
     sex=patients.sex(
         return_expectations={
@@ -60,109 +65,120 @@ study = StudyDefinition(
             "category": {"ratios": {"M": 0.49, "F": 0.5, "U": 0.01}},
         }
     ),
+
     region=patients.registered_practice_as_of(
         "index_date",
         returning="nuts1_region_name",
-        return_expectations={
-            "category": {
-                "ratios": {
-                    "North East": 0.1,
-                    "North West": 0.1,
-                    "Yorkshire and the Humber": 0.1,
-                    "East Midlands": 0.1,
-                    "West Midlands": 0.1,
-                    "East of England": 0.1,
-                    "London": 0.2,
-                    "South East": 0.2,
-                }
-            }
-        },
+        return_expectations={"category": {"ratios": {
+            "North East": 0.1,
+            "North West": 0.1,
+            "Yorkshire and the Humber": 0.1,
+            "East Midlands": 0.1,
+            "West Midlands": 0.1,
+            "East of England": 0.1,
+            "London": 0.2,
+            "South East": 0.2, }}}
     ),
     practice=patients.registered_practice_as_of(
         "index_date",
         returning="pseudo_id",
-        return_expectations={
-            "int": {"distribution": "normal", "mean": 25, "stddev": 5},
-            "incidence": 0.5,
-        },
+        return_expectations={"int" : {"distribution": "normal", "mean": 25, "stddev": 5}, "incidence" : 0.5}
     ),
+
     hepatitis=patients.with_these_clinical_events(
         codelist=hepatitis_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="binary_flag",
-        return_expectations={"incidence": 0.5},
+        return_expectations={
+            "incidence": 0.5
+        },
     ),
+
     gi_illness=patients.with_these_clinical_events(
         codelist=hepatitis_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
-        return_expectations={"incidence": 0.5},
+        between=["index_date", "index_date + 7 days"],
+        return_expectations={
+            "incidence": 0.5
+        },
     ),
+
     alt=patients.with_these_clinical_events(
         codelist=alt_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="binary_flag",
-        return_expectations={"incidence": 0.5},
+        return_expectations={
+            "incidence": 0.5
+        },
     ),
     alt_code=patients.with_these_clinical_events(
         codelist=alt_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="code",
         return_expectations={
             "rate": "universal",
             "category": {"ratios": {"1000731000000107": 0.5, "1000981000000109": 0.5}},
         },
     ),
+    
     alt_numeric_value=patients.with_these_clinical_events(
         codelist=alt_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="numeric_value",
         return_expectations={
             "float": {"distribution": "normal", "mean": 45.0, "stddev": 20},
             "incidence": 0.5,
         },
     ),
+
     ast=patients.with_these_clinical_events(
         codelist=ast_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="binary_flag",
-        return_expectations={"incidence": 0.5},
+        return_expectations={
+            "incidence": 0.5
+        },
     ),
     ast_code=patients.with_these_clinical_events(
         codelist=ast_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="code",
         return_expectations={
             "rate": "universal",
             "category": {"ratios": {"1000731000000107": 0.5, "1000981000000109": 0.5}},
         },
     ),
+    
     ast_numeric_value=patients.with_these_clinical_events(
         codelist=ast_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="numeric_value",
         return_expectations={
             "float": {"distribution": "normal", "mean": 45.0, "stddev": 20},
             "incidence": 0.5,
         },
     ),
+
     bilirubin=patients.with_these_clinical_events(
         codelist=bilirubin_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="binary_flag",
-        return_expectations={"incidence": 0.5},
+        return_expectations={
+            "incidence": 0.5
+        },
     ),
     bilirubin_code=patients.with_these_clinical_events(
         codelist=bilirubin_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="code",
         return_expectations={
             "rate": "universal",
             "category": {"ratios": {"1000731000000107": 0.5, "1000981000000109": 0.5}},
         },
     ),
+    
     bilirubin_numeric_value=patients.with_these_clinical_events(
         codelist=bilirubin_codelist,
-        between=["index_date", "last_day_of_month(index_date)"],
+        between=["index_date", "index_date + 7 days"],
         returning="numeric_value",
         return_expectations={
             "float": {"distribution": "normal", "mean": 45.0, "stddev": 20},
@@ -170,3 +186,4 @@ study = StudyDefinition(
         },
     ),
 )
+
