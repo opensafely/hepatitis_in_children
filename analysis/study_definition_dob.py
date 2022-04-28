717e27cfd24605044f8be90229420565290d0ba4
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-28 12:40:28 +0100

    fix dob study def

--- analysis/study_definition_dob.py
+++ analysis/study_definition_dob.py
@@ -10,31 +10,7 @@ study = StudyDefinition(
         "incidence": 0.1,
     },
     
-    population=patients.satisfying(
-        """
-        registered AND
-        (NOT died) AND
-        (age >=0 AND age <=30)
-        """,
-
-        registered = patients.registered_as_of(
-        "index_date",
-        return_expectations={"incidence": 0.9},
-        ),
-
-        died = patients.died_from_any_cause(
-        on_or_before="index_date",
-        returning="binary_flag",
-        return_expectations={"incidence": 0.1}
-        ),
-        age=patients.age_as_of(
-        "index_date",
-        return_expectations={
-            "rate": "universal",
-            "int": {"distribution": "population_ages"},
-        },
-        ),
-    ),
+    population=patients.all(),
     
     dob=patients.date_of_birth(
     "YYYY-MM",
