12362af2244039f3409190557ba2b038c3406301
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-05-11 14:00:01 +0100

    fix mean values calculation

--- analysis/mean_values.py
+++ analysis/mean_values.py
@@ -7,13 +7,13 @@ from utilities import (
     match_input_files_weekly,
     get_date_input_file,
     get_date_input_file_weekly,
-    redact_small_numbers
+    redact_small_numbers,
 )
 
 Path("output/monthly/joined/redacted").mkdir(parents=True, exist_ok=True)
 Path("output/weekly/joined/redacted").mkdir(parents=True, exist_ok=True)
 
-mean_ages_months = []
+mean_ages_months = {}
 
 for file in (OUTPUT_DIR / "monthly/joined").iterdir():
     if match_input_files(file.name):
@@ -22,28 +22,45 @@ for file in (OUTPUT_DIR / "monthly/joined").iterdir():
         )
         date = get_date_input_file(file.name)
 
-        age_by_group = df.groupby("age_band_months")[["alt_numeric_value", "ast_numeric_value", "bilirubin_numeric_value"]].mean()
-        age_by_group["date"] = date
-        age_by_group["population"] = df.groupby("age_band_months").size()
+        for test in ["alt", "ast", "bilirubin"]:
+            mean_ages_months[test] = []
+            # only want those who have had a test
+            df_subset = df.loc[df[test] == 1, :]
 
-        for i in ["alt", "ast", "bilirubin"]:
-            age_by_group[f"{i}_numeric_value_count"] = df.groupby("age_band_months")[[f"{i}_numeric_value"]].count()
-        mean_ages_months.append(age_by_group)
-        
+            age_by_group = df_subset.groupby("age_band_months")[
+                [f"{test}_numeric_value"]
+            ].mean()
 
-mean_ages_months_concat = pd.concat(mean_ages_months)
+            age_by_group["date"] = date
+            age_by_group["population"] = df.groupby("age_band_months").size()
 
-# redact any mean values where calculated from low n
+            age_by_group[f"{test}_numeric_value_count"] = df.groupby("age_band_months")[
+                [f"{test}_numeric_value"]
+            ].count()
 
-mean_ages_months_concat = redact_small_numbers(mean_ages_months_concat, 5, "alt_numeric_value_count", "population", "alt_numeric_value", "date", "age_band_months")
-mean_ages_months_concat = redact_small_numbers(mean_ages_months_concat, 5, "ast_numeric_value_count", "population", "ast_numeric_value", "date", "age_band_months")
-mean_ages_months_concat = redact_small_numbers(mean_ages_months_concat, 5, "bilirubin_numeric_value_count", "population", "bilirubin_numeric_value", "date", "age_band_months")
+            mean_ages_months[test].append(age_by_group)
 
-mean_ages_months_concat.to_csv(OUTPUT_DIR / "monthly/joined/redacted/mean_test_value_by_age.csv")
+for test in ["alt", "ast", "bilirubin"]:
+    mean_ages_months_concat = pd.concat(mean_ages_months[test])
+
+    # redact any mean values where calculated from low n
+
+    mean_ages_months_concat = redact_small_numbers(
+        mean_ages_months_concat,
+        5,
+        f"{test}_numeric_value_count",
+        "population",
+        f"{test}_numeric_value",
+        "date",
+        "age_band_months",
+    )
+
+    mean_ages_months_concat.to_csv(
+        OUTPUT_DIR / "monthly/joined/redacted/mean_test_value_{test}_by_age.csv"
+    )
 
 
-
-mean_ages_weeks = []
+mean_ages_weeks = {}
 
 for file in (OUTPUT_DIR / "weekly/joined").iterdir():
     if match_input_files_weekly(file.name):
@@ -52,21 +69,40 @@ for file in (OUTPUT_DIR / "weekly/joined").iterdir():
         )
         date = get_date_input_file_weekly(file.name)
 
-        age_by_group = df.groupby("age_band_months")[["alt_numeric_value", "ast_numeric_value", "bilirubin_numeric_value"]].mean()
-        age_by_group["date"] = date
-        age_by_group["population"] = df.groupby("age_band_months").size()
+        for test in ["alt", "ast", "bilirubin"]:
 
-        for i in ["alt", "ast", "bilirubin"]:
-            age_by_group[f"{i}_numeric_value_count"] = df.groupby("age_band_months")[[f"{i}_numeric_value"]].count()
-        mean_ages_weeks.append(age_by_group)
-        
+            mean_ages_weeks[test] = []
+            # only want those who have had a test
+            df_subset = df.loc[df[test] == 1, :]
 
-mean_ages_weeks_concat = pd.concat(mean_ages_weeks)
+            age_by_group = df_subset.groupby("age_band_months")[
+                [f"{test}_numeric_value"]
+            ].mean()
 
-# redact any mean values where calculated from low n
+            age_by_group["date"] = date
+            age_by_group["population"] = df.groupby("age_band_months").size()
 
-mean_ages_weeks_concat = redact_small_numbers(mean_ages_weeks_concat, 5, "alt_numeric_value_count", "population", "alt_numeric_value", "date", "age_band_months")
-mean_ages_weeks_concat = redact_small_numbers(mean_ages_weeks_concat, 5, "ast_numeric_value_count", "population", "ast_numeric_value", "date", "age_band_months")
-mean_ages_weeks_concat = redact_small_numbers(mean_ages_weeks_concat, 5, "bilirubin_numeric_value_count", "population", "bilirubin_numeric_value", "date", "age_band_months")
+            age_by_group[f"{test}_numeric_value_count"] = df.groupby("age_band_months")[
+                [f"{test}_numeric_value"]
+            ].count()
 
-mean_ages_weeks_concat.to_csv(OUTPUT_DIR / "weekly/joined/redacted/mean_test_value_by_age.csv")
+            mean_ages_weeks[test].append(age_by_group)
+
+for test in ["alt", "ast", "bilirubin"]:
+    mean_ages_weeks_concat = pd.concat(mean_ages_weeks[test])
+
+    # redact any mean values where calculated from low n
+
+    mean_ages_weeks_concat = redact_small_numbers(
+        mean_ages_weeks_concat,
+        5,
+        f"{test}_numeric_value_count",
+        "population",
+        f"{test}_numeric_value",
+        "date",
+        "age_band_months",
+    )
+
+    mean_ages_weeks_concat.to_csv(
+        OUTPUT_DIR / "weekly/joined/redacted/mean_test_value_{test}_by_age.csv"
+    )
