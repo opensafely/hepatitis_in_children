5fbf7d443fa14def78011f968d95fea74ecdda5e
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-05-12 18:12:31 +0100

    fix file matching

--- analysis/isaric_preprocess.py
+++ analysis/isaric_preprocess.py
@@ -3,11 +3,8 @@ import numpy as np
 from pathlib import Path
 from utilities import (
     OUTPUT_DIR,
-    match_input_files,
     match_input_files_weekly,
-    get_date_input_file,
     get_date_input_file_weekly,
-    redact_small_numbers,
 )
 
 Path("output/monthly/joined/isaric").mkdir(parents=True, exist_ok=True)
@@ -18,17 +15,17 @@ mean_ages_months = {}
 rows = []
 
 for file in (OUTPUT_DIR / "weekly/joined").iterdir():
-    if match_input_files(file.name):
+    if match_input_files_weekly(file.name):
         df = pd.read_csv(
             (OUTPUT_DIR / "weekly/joined") / file.name, parse_dates=["dob"]
         )
-        date = get_date_input_file(file.name)
+        date = get_date_input_file_weekly(file.name)
         
         #subset the columns we're interested in
         df = df.loc[((df["alt_numeric_value_date"].notnull()) | (df["ast_numeric_value_date"].notnull())), ["patient_id", "dob", "alt_numeric_value_date", "ast_numeric_value_date", "alt_numeric_value", "ast_numeric_value"]]
        
         unique_dates = np.unique(list(df["alt_numeric_value_date"].values) + list(df["ast_numeric_value_date"].values))
-        print(date)
+        
         for d in unique_dates:
           
             df_subset = df.loc[(df["alt_numeric_value_date"] == d) | (df["ast_numeric_value_date"] == d),:]
